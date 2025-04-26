5f9f9ba08359471f47121fc0accd2284b24d0eb7
Ryan Hillis <ryanbhillis@gmail.com>
2025-04-25 20:45:43 -0400

    api.js changes

--- api.js
+++ api.js
@@ -2,6 +2,8 @@ class APIConnection {
   constructor(email, password) {
     this.URL = "http://ec2-18-189-105-20.us-east-2.compute.amazonaws.com/api/";
     this.token = "";
+    this.email = email;
+    this.password = password;
   }
 
   async request(url) {
@@ -16,18 +18,24 @@ class APIConnection {
     return await res;
   }
 
-  async login(email, password) {
+  async login() {
     const res = await fetch(this.URL + "auth/login", {
       method: "POST",
       headers: {
-        "Content-Type": "text/plain",
+        "Content-Type": "application/json",
         Accept: "*/*",
       },
-      body: JSON.stringify({ email, password }),
+      body: `{
+        "email": "${this.email}",
+        "password": "${this.password}"
+      }`,
     });
 
     await console.log(res);
 
     this.token = await res.json().token;
+    return this.token;
   }
 }
+
+api = new APIConnection("ryanbhillis@gmail.com", "mypasswordsuXXX:3");
