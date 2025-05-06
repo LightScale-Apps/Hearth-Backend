a8484e769359041851cc8669be800c4f2eab2a09
Ryan Hillis <ryanbhillis@gmail.com>
2025-05-06 01:37:01 -0400

    publish

---
+++ api/Models/RefreshTokens.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Identity.Client;
+
+namespace api.Models
+{
+    [Table("RefreshTokens")]    
+    public class RefreshTokens
+    {
+        public int Id {get; set;}
+        public string Token {get; set;} = string.Empty;
+        public string OwnedBy {get; set;} = string.Empty;
+        public DateTime CreatedOn {get; set;} = DateTime.Now;
+        
+    }
+}
