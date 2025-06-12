3fc4cf7c73999a436efd159674f7bf0897ad9098
Ryan Hillis <ryanbhillis@gmail.com>
2025-06-12 11:55:37 -0400

    semicolon

--- api/Service/ChatService.cs
+++ api/Service/ChatService.cs
@@ -1,14 +1,13 @@
 using System;
 using System.Collections.Generic;
-using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
-using System.Security.Claims;
 using System.Text;
+using System.Net.WebSockets;
 using System.Threading.Tasks;
 using api.Interfaces;
 using api.Models;
-using Microsoft.IdentityModel.Tokens;
-using System.Net.WebSockets;
+using api.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace api.Service {
     public class ChatService : IChatService
